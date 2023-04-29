58039b683c3cc10a56ab11d6b21430834727e2b5
Liron <lironcohen3@mail.tau.ac.il>
2023-04-29 21:32:52 +0300

    added files

---
+++ iteration2/StatisticalModelPerDescription.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import spacy
+import difflib
+
+# load the spaCy model
+nlp = spacy.load("en_core_web_lg")
+
+# read in the job descriptions excel
+df = pd.read_excel("job_descriptions.xlsx")
+
+# read in the biased keyphrases excel
+bias_df = pd.read_excel("biased_keywords.xlsx")
+
+# conert to lowercase
+bias_df['phrase'] = bias_df['phrase'].str.lower()
+
+# create a set of all biased phrases (exact matches)
+exact_biased_phrases = set(bias_df['phrase'].tolist())
+
+# define a function to check if a description contains any biased phrases (exact or similar matches)
+def check_for_bias(description):
+    description = description.lower()
+    desc_tokens = [token.text for token in nlp(description.lower())]
+    matches = {'Skills': [], 'work environment': [], 'nlp_phrases': [], 'education': [], 'coding lang': [] }
+    for phrase in exact_biased_phrases:
+      if phrase.lower() in description:
+            category = bias_df[bias_df['phrase']==phrase]['category'].values[0] # very inefficient
+            matches[category].append(phrase)
+      else:
+          closest_match = difflib.get_close_matches(phrase, desc_tokens, n=1, cutoff=0.8)
+          if closest_match:
+              matches['nlp_phrases'].append(closest_match[0])
+    return matches
+
+# create a new column indicating which biased phrases are present
+df['matches'] = df['description'].apply(check_for_bias)
+
+# create a new dataframe to store the results
+results_df = pd.DataFrame(columns=['description'])
+
+# iterate over the job descriptions to calculate the percentage of biased phrases present in each
+for index, row in df.iterrows():
+    description = row['description']
+
+    skills_biased_phrases = set(row['matches']['Skills'])
+    work_env_biased_phrases = set(row['matches']['work environment'])
+    coding_lang_biased_phrases = set(row['matches']['coding lang'])
+    education_biased_phrases = set(row['matches']['education'])
+    nlp_biased_phrases = set(row['matches']['nlp_phrases'])
+
+    nlp_biased_phrases -= skills_biased_phrases
+    nlp_biased_phrases -= work_env_biased_phrases
+    nlp_biased_phrases -= coding_lang_biased_phrases
+    nlp_biased_phrases -= education_biased_phrases
+
+    skills_count = len(skills_biased_phrases)
+    work_env_count = len(work_env_biased_phrases)
+    coding_lang_count = len(coding_lang_biased_phrases)
+    education_count = len(education_biased_phrases)
+    nlp_count = len(nlp_biased_phrases)
+    total_count = skills_count + work_env_count + coding_lang_count + education_count + nlp_count
+
+
+    results_df = results_df.append({'description': description, 'skills_biased_phrases': skills_biased_phrases, 'skills_count': skills_count, 'work_env_biased_phrases': work_env_biased_phrases, 'work_env_count': work_env_count,'coding_lang_biased_phrases': coding_lang_biased_phrases,"coding_lang_count": coding_lang_count, 'education_biased_phrases': education_biased_phrases, 'education_count': education_count, 'nlp_biased_phrases': nlp_biased_phrases, 'nlp_count': nlp_count, 'total_count': total_count}, ignore_index=True)
+
+# save the results to a new excel file
+results_df.to_excel("bias_analysis_results.xlsx")
+
---
+++ iteration2/StatisticalModelPerPhrase.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import spacy
+import difflib
+
+# load the spaCy model
+nlp = spacy.load("en_core_web_lg")
+
+# read in the job descriptions excel
+df = pd.read_excel("job_descriptions_2.xlsx")
+
+# read in the biased keyphrases excel
+bias_df = pd.read_excel("biased_keywords.xlsx")
+
+# create a set of all biased phrases (exact matches)
+exact_biased_phrases = set(bias_df['phrase'].tolist())
+
+# create a dictionary of all biased phrases (similar matches) and their corresponding categories
+similar_biased_phrases = {}
+for index, row in bias_df.iterrows():
+    category = row['category']
+    phrase = row['phrase']
+    if phrase not in exact_biased_phrases:
+        if category not in similar_biased_phrases:
+            similar_biased_phrases[category] = [phrase]
+        else:
+            similar_biased_phrases[category].append(phrase)
+
+# define a function to check if a description contains any biased phrases (exact or similar matches)
+def check_for_bias(description):
+    doc = nlp(description.lower())
+    matches = []
+    for token in doc:
+        token_matches = {'phrase': token.text, 'category': None, 'original': True}
+        if token.text in exact_biased_phrases:
+            token_matches['category'] = bias_df[bias_df['phrase']==token.text]['category'].values[0]
+            matches.append(token_matches)
+        else:
+            for category in similar_biased_phrases:
+                closest_match = difflib.get_close_matches(token.text, similar_biased_phrases[category], n=1, cutoff=0.3)
+                if closest_match:
+                    token_matches['category'] = category
+                    token_matches['phrase'] = closest_match[0]
+                    token_matches['original'] = False
+                    matches.append(token_matches)
+    return matches
+
+# create a new column in the job descriptions excel indicating which biased phrases are present
+df['biased_phrases'] = df['description'].apply(check_for_bias)
+
+# create a dictionary of all biased phrases and their corresponding categories
+all_biased_phrases = {}
+for index, row in bias_df.iterrows():
+    category = row['category']
+    phrase = row['phrase']
+    all_biased_phrases[phrase] = category
+
+# calculate the percentage of job descriptions that contain each biased phrase
+counts = {}
+for index, row in df.iterrows():
+    for phrase in row['biased_phrases']:
+        if phrase['phrase'] not in counts:
+            counts[phrase['phrase']] = {'count': 1, 'percent': 0, 'category': phrase['category'], 'original': phrase['original']}
+        else:
+            counts[phrase['phrase']]['count'] += 1
+for phrase in counts:
+    counts[phrase]['percent'] = counts[phrase]['count'] / len(df) * 100
+
+# create a new dataframe to store the results
+results_df = pd.DataFrame.from_dict(counts, orient='index', columns=['count', 'percent', 'category', 'original'])
+
+# save the results to a new excel file
+results_df.to_excel("bias_analysis_results.xlsx")
+
+print("Results saved to bias_analysis_results.xlsx")
